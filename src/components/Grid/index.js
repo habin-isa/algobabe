5cbfefeb4f21f8f7da2cbeba819ba0f6cc3265be
ferraripuppi <habin@hotmail.co.uk>
2020-03-16 22:01:06 +0000

    adds soundpack + cell + grid components

---
+++ src/components/Grid/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as S from './styles';
+import Cell from '../Cell';
+
+const Grid = ({ sequence, toggleStep }) => (
+  <S.Frame rows={4} columns={16}>
+    {sequence.map((line, i) =>
+      line.map((time, j) => (
+        <Cell
+          key={i + j}
+          column={j + 1}
+          row={i + 1}
+          activated={sequence[i][j]['activated']}
+          triggered={sequence[i][j]['triggered']}
+          onClick={() => toggleStep(i, j)}
+        />
+      ))
+    )}
+  </S.Frame>
+);
+
+export default Grid;
